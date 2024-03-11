5af2ecbe3d8044b2feee84bc54ca3e39a41c2878
Ralph Urlus <rurlus.dev@gmail.com>
2024-03-11 17:51:22 +0100

    feat(viz): Add scatter plot with binary labels

--- src/ppu/viz/__init__.py
+++ src/ppu/viz/__init__.py
@@ -4,6 +4,12 @@
 from ppu.viz.color import set_plot_style
 from ppu.viz.contours import plot_pdf_contours
 from ppu.viz.ellipse import plot_ellipse_from_cov
-from ppu.viz.scatter import plot_dense_scatter
+from ppu.viz.scatter import plot_dense_binary_scatter, plot_dense_scatter
 
-__all__ = ["plot_ellipse_from_cov", "plot_pdf_contours", "set_plot_style", "plot_dense_scatter"]
+__all__ = [
+    "plot_ellipse_from_cov",
+    "plot_pdf_contours",
+    "set_plot_style",
+    "plot_dense_binary_scatter",
+    "plot_dense_scatter",
+]
--- src/ppu/viz/scatter.py
+++ src/ppu/viz/scatter.py
@@ -5,15 +5,30 @@ from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
 
+from ppu.viz.legend import LegendHandler
+
 if TYPE_CHECKING:
     from numpy.typing import NDArray
 
-__all__ = ["plot_dense_scatter"]
+__all__ = ["plot_dense_binary_scatter", "plot_dense_scatter"]
 
 
 def plot_dense_scatter(
     X: NDArray, ax: plt.Axes | None = None, color: str | None = None, alpha: float = 0.8, zorder: int = 1, **kwargs
 ):
     ax = ax or plt.gca()
-    ax.scatter(X[:, 0], X[:, 1], s=1, marker="x", c=color, alpha=alpha, zorder=zorder, **kwargs)
+    s = kwargs.get("s", 1)
+    ax.scatter(X[:, 0], X[:, 1], s=s, marker="x", c=color, alpha=alpha, zorder=zorder, **kwargs)
+    return ax
+
+
+def plot_dense_binary_scatter(
+    X: NDArray, y: NDArray, ax: plt.Axes | None = None, alpha: float = 0.8, zorder: int = 1, **kwargs
+):
+    ax = ax or plt.gca()
+    lh = LegendHandler(ax)
+    mask = y.astype(bool)
+    ax.scatter(X[~mask, 0], X[~mask, 1], marker=".", s=30, c="C0", label="$y=0$", alpha=alpha, zorder=zorder, **kwargs)
+    ax.scatter(X[mask, 0], X[mask, 1], marker="x", s=15, c="C1", label="$y=1$", alpha=alpha, zorder=zorder, **kwargs)
+    lh.add_legend(fontsize=12)
     return ax
