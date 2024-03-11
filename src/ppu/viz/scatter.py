765142f913e1c165899fd10b6d94dd0d519e1ff2
Ralph Urlus <rurlus.dev@gmail.com>
2024-03-11 15:28:10 +0100

    feat(viz): Add additional plotting funcs to Bivariate Gaussian generator

--- src/ppu/viz/scatter.py
+++ src/ppu/viz/scatter.py
@@ -25,10 +25,18 @@ def plot_dense_scatter(
 def plot_dense_binary_scatter(
     X: NDArray, y: NDArray, ax: plt.Axes | None = None, alpha: float = 0.8, zorder: int = 1, **kwargs
 ):
+    s = kwargs.get("s", (10, 15))
+    if isinstance(s, (int, float)):
+        s0 = s1 = s
+    elif isinstance(s, (list, tuple)):
+        s0, s1 = s
+    else:
+        msg = "`s` should be scalar or tuple of scalars."
+        raise TypeError(msg)
     ax = ax or plt.gca()
     lh = LegendHandler(ax)
     mask = y.astype(bool)
-    ax.scatter(X[~mask, 0], X[~mask, 1], marker=".", s=30, c="C0", label="$y=0$", alpha=alpha, zorder=zorder, **kwargs)
-    ax.scatter(X[mask, 0], X[mask, 1], marker="x", s=15, c="C1", label="$y=1$", alpha=alpha, zorder=zorder, **kwargs)
+    ax.scatter(X[~mask, 0], X[~mask, 1], marker=".", s=s0, c="C0", label="$y=0$", alpha=alpha, zorder=zorder, **kwargs)
+    ax.scatter(X[mask, 0], X[mask, 1], marker="+", s=s1, c="C1", label="$y=1$", alpha=alpha, zorder=zorder, **kwargs)
     lh.add_legend(fontsize=12)
     return ax
