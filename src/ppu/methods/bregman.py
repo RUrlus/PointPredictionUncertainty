04d402bfb19409536dff8813fdecbc52a5fb5c75
Ralph Urlus <rurlus.dev@gmail.com>
2024-03-12 12:41:31 +0100

    pkg: Add placeholder module for methods

---
+++ src/ppu/methods/bregman.py
@@ -0,0 +1,6 @@
+"""Module with Bregman Information implementation.
+
+Based on:
+Uncertainty Estimates of Predictions via a General Bias-Variance Decomposition
+Sebastian G. Gruber and Florian Buettner
+"""
