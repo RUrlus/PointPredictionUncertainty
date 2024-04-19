62ba0722a8a746adc8fbb85ba5de5539cebe22b9
zchlewis <2385327417@qq.com>
2024-04-19 22:17:18 +0200

    feat: add grid variance and mean distance BI

--- src/ppu/methods/bregman.py
+++ src/ppu/methods/bregman.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from matplotlib.colors import ListedColormap
+from scipy.special import softmax
+from scipy.stats import norm
 from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.inspection._plot.decision_boundary import _check_boundary_response_method
 
@@ -21,13 +23,26 @@ from ppu.methods.utils import BI_LSE, get_dataset, stable_logit_transform
 
 def get_BI(xs, models):
     preds = np.array([_check_boundary_response_method(m, "auto")(xs) for m in models])
-    # preds might be probabilities
+    # preds are probabilities
     if len(preds.shape) == 3:
         preds = preds[:,:,1]
 
     logits = stable_logit_transform(preds)
 
     BIs = np.array([BI_LSE(zs, bound="lower") for zs in logits.T])
+    return BIs
+
+def get_revised_BI(xs, models):
+    preds = np.array([_check_boundary_response_method(m, "auto")(xs) for m in models])
+    # preds are probabilities
+    if len(preds.shape) == 3:
+        preds = preds[:,:,1]
+
+    logits = stable_logit_transform(preds)
+
+    BIs = np.array([BI_LSE(zs, bound="lower") / abs(b.mean() - 0.5) for b,zs in zip(preds.T, logits.T)])
+    # BIs = np.array([BI_LSE(zs, bound="lower") / (-0.05 / (abs(b.mean() - 0.5) + 0.07) + 1) for b,zs in zip(preds.T, logits.T)])
+    # BIs = np.array(softmax([10*(1 - norm.cdf(abs(b.mean() - 0.5) / b.var()))-1. for b,zs in zip(preds.T, logits.T)]))
     return BIs
 
 def get_models(clf, gen, reps, n_samples=200, **kwargs):
